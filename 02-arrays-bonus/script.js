817025b4a6d305f825c3b9b1993d520d6dbdde40
alexciccarese <alex.ciccarese@gmail.com>
2025-02-12 16:40:42 +0100

    add all teachers in a string by .join

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -39,4 +39,5 @@ if (teachers.includes(isFabioPresent)) {
 
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
-const teachersString = null;
+const teachersString = teachers.join();
+console.log(teachersString);
